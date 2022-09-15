604322cea5d87991d45142d69795206187030bc8
sangho <hop7311@naver.com>
2022-09-15 17:42:54 +0900

    update model.py

--- AlexNet/model.py
+++ AlexNet/model.py
@@ -6,40 +6,42 @@ class AlexNet(nn.Module):
     def __init__(
         self,
         in_dim=3,
+        filters=[96, 256, 384, 384, 256],
         num_classes=1000,
+        dropout=0.5,
     ):
         super(AlexNet, self).__init__()
         
         self.features = nn.Sequential(
-            nn.Conv2d(in_dim, 96, kernel_size=11, stride=4, padding=2),
+            nn.Conv2d(in_dim, filters[0], kernel_size=11, stride=4, padding=2),
             nn.ReLU(),
             nn.LocalResponseNorm(size=5, alpha=0.0001, beta=0.75, k=2),
             nn.MaxPool2d(kernel_size=3, stride=2),
             
-            nn.Conv2d(96, 256, kernel_size=5, stride=1, padding=2),
+            nn.Conv2d(filters[0], filters[1], kernel_size=5, stride=1, padding=2),
             nn.ReLU(),
             nn.LocalResponseNorm(size=5, alpha=0.0001, beta=0.75, k=2),
             nn.MaxPool2d(kernel_size=3, stride=2), 
             
-            nn.Conv2d(256, 384, kernel_size=3, stride=1, padding=1),
+            nn.Conv2d(filters[1], filters[2], kernel_size=3, stride=1, padding=1),
             nn.ReLU(),
             
-            nn.Conv2d(384, 384, kernel_size=3, stride=1, padding=1),
+            nn.Conv2d(filters[2], filters[3], kernel_size=3, stride=1, padding=1),
             nn.ReLU(),
             
-            nn.Conv2d(384, 256, kernel_size=3, stride=1, padding=1),
+            nn.Conv2d(filters[3], filters[4], kernel_size=3, stride=1, padding=1),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=3, stride=2),
         )
         
         self.classifier = nn.Sequential(
-            nn.Linear(256*6*6, 4096),
+            nn.Linear(filters[4]*6*6, 4096),
             nn.ReLU(),
-            nn.Dropout(0.5),
+            nn.Dropout(dropout),
         
             nn.Linear(4096, 4096),
             nn.ReLU(),
-            nn.Dropout(0.5),
+            nn.Dropout(dropout),
         
             nn.Linear(4096, num_classes),
         )
