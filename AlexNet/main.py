b797990420df7023a350fcd0e48475f1777518c4
sangho <hop7311@naver.com>
2022-09-19 22:31:42 +0900

    update main.py to train model

--- AlexNet/main.py
+++ AlexNet/main.py
@@ -3,18 +3,23 @@ import argparse
 import torch
 import torch.nn as nn
 import torchvision.transforms as transforms
+import torchvision.datasets as dset
 from torch.utils.data import DataLoader
 from torchvision.datasets import ImageFolder
 from torchsummary import summary
 
 from dataset import CustomDataset
-from model import AlexNet
+from model import get_alexnet
 from train import TrainModel
 
 def get_args_parser():
     parser = argparse.ArgumentParser(description='Training AlexNet', add_help=False)
-    parser.add_argument('--data_dir', type=str, required=True,
-                        help='directory where your dataset is located')
+    parser.add_argument('--use_benchmark', type=bool, required=True,
+                        help='if you use benchmark (ex: cifar10), then write True, or False')
+    parser.add_argument('--data_dir', type=str, default=None,
+                        help='directory where your dataset is located, write it if you set use_benchmakr to False')
+    parser.add_argument('--pretrained', type=bool, default=True,
+                        help='use pretrained model')
     parser.add_argument('--resize_size', type=int, default=256,
                         help='image resize size before applying cropping')
     parser.add_argument('--crop_size', type=int, default=224,
@@ -27,10 +32,12 @@ def get_args_parser():
                         help='Batch Size for training model')
     parser.add_argument('--weight_decay', default=5e-4, type=float,
                         help='weight decay of optimizer SGD')
-    parser.add_argument('--num_classes', type=int, default=2,
+    parser.add_argument('--num_classes', type=int, default=10,
                         help='class number of dataset')
     parser.add_argument('--lr_scheduling', default=True, type=bool,
                         help='apply learning rate scheduler')
+    parser.add_argument('--check_point', default=True, type=bool,
+                        help='save weight file when achieve the best score in validation phase')
     parser.add_argument('--train_log_step', type=int, default=40,
                         help='print log of iteration in training loop')
     parser.add_argument('--valid_log_step', type=int, default=10,
@@ -38,11 +45,15 @@ def get_args_parser():
     return parser
 
 def main(args):
+    assert (args.use_benchmark==True and args.data_dir is None) or \
+        (args.use_benchmark==False and args.data_dir is not None)
+
     # Load dataset and Apply augmentation options
     train_transforms_ = transforms.Compose([
         transforms.Resize((args.resize_size, args.resize_size)),
         transforms.RandomCrop((args.crop_size, args.crop_size)),
         transforms.RandomHorizontalFlip(p=0.5),
+        transforms.ColorJitter(brightness=0.5, contrast=0.5, saturation=0.5),
         transforms.ToTensor(),
         transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)),
     ])
@@ -53,46 +64,70 @@ def main(args):
         transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)),
     ])
 
-    train_folder = ImageFolder(
-        root=args.data_dir+'/train',
-        transform=train_transforms_,
-    )
+    if args.use_benchmark:
+        train_data = dset.CIFAR10(
+            root='./data', 
+            train=True, 
+            download=True, 
+            transform=train_transforms_
+        )
 
-    valid_folder = ImageFolder(
-        root=args.data_dir+'/valid',
-        transform=valid_transforms_,
-    )
+        train_loader = DataLoader(
+            train_data,
+            batch_size=16,
+            shuffle=True,
+            drop_last=True,
+        )
 
-    train_loader = DataLoader(
-        train_folder,
-        batch_size=args.batch_size,
-        shuffle=True,
-        drop_last=True,
-    )
+        test_data = dset.CIFAR10(
+            root='./data',
+            train=False,
+            download=True,
+            transform=valid_transforms_,
+        )
 
-    valid_loader = DataLoader(
-        valid_folder,
-        batch_size=args.batch_size,
-        shuffle=True,
-        drop_last=True,
-    )
+        test_loader = DataLoader(
+            test_data,
+            batch_size=16,
+            shuffle=False,
+            drop_last=True,
+        )
 
-    # train_loader = DataLoader(
-    #     CustomDataset(path=args.data_dir, subset='train', transforms_=train_transforms_),
-    #     batch_size=args.batch_size,
-    #     shuffle=True,
-    #     drop_last=True,
-    # )
+    else:
+        train_folder = ImageFolder(
+            root=args.data_dir+'/train',
+            transform=train_transforms_,
+        )
 
-    # valid_loader = DataLoader(
-    #     CustomDataset(path=args.data_dir, subset='valid', transforms_=valid_transforms_),
-    #     batch_size=args.batch_size,
-    #     shuffle=True,
-    #     drop_last=True,
-    # )
+        valid_folder = ImageFolder(
+            root=args.data_dir+'/valid',
+            transform=valid_transforms_,
+        )
+
+        train_loader = DataLoader(
+            train_folder,
+            batch_size=args.batch_size,
+            shuffle=True,
+            drop_last=True,
+        )
+
+        valid_loader = DataLoader(
+            valid_folder,
+            batch_size=args.batch_size,
+            shuffle=True,
+            drop_last=True,
+        )
 
     # Load AlexNet and check summary
-    alexnet = AlexNet(num_classes=args.num_classes)
+    if args.pretrained:
+        if args.use_benckmark:
+            alexnet = get_alexnet(pretrained=True, num_classes=10)
+        else:
+            alexnet = get_alexnet(pretrained=True, num_classes=args.num_classes)
+    
+    else:
+        alexnet = get_alexnet(pretrained=False, num_classes=args.num_classes)
+
     summary(alexnet, (3, args.crop_size, args.crop_size), device='cpu')
 
     model = TrainModel(
@@ -101,6 +136,7 @@ def main(args):
         epochs=args.epochs,
         weight_decay=args.weight_decay,
         lr_scheduling=args.lr_scheduling,
+        check_point=args.check_point,
         train_log_step=args.train_log_step,
         valid_log_step=args.valid_log_step,
     )
